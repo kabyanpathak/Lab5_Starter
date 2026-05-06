7de497857f4f8588d613ddecfdef770c396a653b
kabyanpathak <kabyan388@gmail.com>
2026-05-05 23:08:15 -0700

    completed all steps

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,15 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('valid phone number with dashes', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('valid phone number with parentheses', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('invalid phone number - too short', () => {
+  expect(isPhoneNumber('123-4567')).toBe(false);
+});
+test('invalid phone number - letters', () => {
+  expect(isPhoneNumber('abc-def-ghij')).toBe(false);
+});
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,38 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const horn = document.getElementById('horn-select');
+  const img = document.querySelector('#expose img');
+  const audio = document.querySelector('audio');
+  const volume_slide = document.getElementById('volume');
+  const volume_controls = document.querySelector('#volume-controls img');
+  const play_button = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+
+  horn.addEventListener('change', () => {
+    img.src = `assets/images/${horn.value}.svg`;
+    audio.src = `assets/audio/${horn.value}.mp3`;
+  });
+
+  volume_slide.addEventListener('input', () => {
+    const vol = volume_slide.value;
+    audio.volume = vol / 100; 
+    if (vol == 0) {
+      volume_controls.src = 'assets/icons/volume-level-0.svg';
+    } else if (vol < 33) {
+      volume_controls.src = 'assets/icons/volume-level-1.svg';
+    } else if (vol < 67) {
+      volume_controls.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volume_controls.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  play_button.addEventListener('click', () => {
+    audio.play();
+    if (horn.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
